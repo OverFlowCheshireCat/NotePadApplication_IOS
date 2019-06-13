e1ad38b09010bd816395a0c5bc50c6400e45bf63
dit03 <dit@imac-4.local>
2019-06-13 18:49:34 +0900

    Finish Source

--- NotePadApplication_IOS/DetailViewController.swift
+++ NotePadApplication_IOS/DetailViewController.swift
@@ -1,9 +1,20 @@
 import UIKit
+import RealmSwift
+
+class MemoData: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var content = ""
+}
 
 class DetailViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     
     @IBOutlet weak var head: UITextField!
     @IBOutlet weak var content: UITextView!
+    
+    public static var realm : Realm? = nil
+    public static var titleValue = ""
+    public static var contentValue = ""
+    public static var index = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +27,16 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UITextViewDel
         
         content.layer.borderWidth = 1.0
         content.layer.borderColor = UIColor.gray.cgColor
+
+        if DetailViewController.titleValue != "" && DetailViewController.contentValue != "" {
+            var memos = ViewController.reloadMemo(realm: DetailViewController.realm!)
+            
+            head.text = DetailViewController.titleValue
+            content.text = DetailViewController.contentValue
+            
+            DetailViewController.titleValue = ""
+            DetailViewController.contentValue = ""
+        }
     }
     
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -34,12 +55,38 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UITextViewDel
         return true
     }
     
+    func addMemo(memoData : MemoData)  {
+        try! DetailViewController.realm?.write {
+            DetailViewController.realm?.add(memoData)
+        }
+    }
+    
+    func updateMemo(memoData : MemoData, titleV : String, contentV : String) {
+        try! DetailViewController.realm?.write {
+            memoData.title = titleV
+            memoData.content = contentV
+        }
+    }
+    
     @IBAction func saveMemo(_ sender: Any) {
-        ViewController.titles.append(head.text!)
-        ViewController.contents.append(content.text)
+        let memoData = MemoData()
+        
+        memoData.title = head.text!
+        memoData.content = content.text
+        
+        if DetailViewController.index != -1 {
+            var memos = ViewController.reloadMemo(realm: DetailViewController.realm!)
+            
+            updateMemo(memoData: memos[DetailViewController.index], titleV: head.text!, contentV: content.text)
+
+            DetailViewController.index = -1
+        } else {
+            addMemo(memoData: memoData)
+        }
         
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let view = storyboard.instantiateInitialViewController()
+        
         self.present(view!, animated: true, completion: nil)
     }
 }
--- NotePadApplication_IOS/ViewController.swift
+++ NotePadApplication_IOS/ViewController.swift
@@ -1,46 +1,77 @@
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var mTimer = Timer()
-    
-    public static var titles : [String] = []
-    public static var contents : [String] = []
+    var realm = try! Realm()
         
     public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "메모장 목록"
     }
-        
+    
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ViewController.titles.count
+        var memos = ViewController.reloadMemo(realm: realm)
+        
+        return memos.count
     }
         
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Re", for: indexPath)
         let row = indexPath.row
-            
-        cell.textLabel?.text = ViewController.titles[row]
+        
+        var memos = ViewController.reloadMemo(realm: realm)
+        
+        cell.textLabel?.text = memos[row].title
             
         return cell
     }
         
     public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        ViewController.titles.remove(at: indexPath.row)
-        ViewController.contents.remove(at: indexPath.row)
-            
+        var memos = ViewController.reloadMemo(realm: realm)
+
+        deleteMemo(memoData: memos[indexPath.row])
+                
         tableView.reloadData()
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var memos = ViewController.reloadMemo(realm: realm)
+        
+        DetailViewController.realm = realm
+        DetailViewController.titleValue = memos[indexPath.row].title
+        DetailViewController.contentValue = memos[indexPath.row].content
+        DetailViewController.index = indexPath.row
+    }
+    
+    public static func reloadMemo(realm : Realm) -> Results<MemoData> {
+        var memos: Results<MemoData> {
+            
+            get {
+                return realm.objects(MemoData.self)
+            }
+        }
+        
+        return memos
+    }
+    
+    func deleteMemo(memoData : MemoData)  {
+        try! realm.write {
+            realm.delete(memoData)
+        }
+    }
+    
+    @IBAction func writeMemo(_ sender: Any) {
+        DetailViewController.titleValue = ""
+        DetailViewController.contentValue = ""
+        DetailViewController.index = -1
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        
         tableView.delegate = self
         tableView.dataSource = self
-        
-        mTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { timer in
-            self.tableView.reloadData()
-        })
     }
 }
